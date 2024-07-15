bc77442e7ce9899f829930023e6ccc6eb36e3d53
Nick-Eagles <nickeagles77@gmail.com>
2024-07-15 12:49:56 -0400

    Adapt deploy script for this project

--- code/05_shiny/deploy.R
+++ code/05_shiny/deploy.R
@@ -1,6 +1,8 @@
 library("rsconnect")
 library("here")
 
+spe_path = here('processed-data', '04_example_data', 'Visium_LS_spe.rds')
+
 ## Or you can go to your shinyapps.io account and copy this
 ## Here we do this to keep our information hidden.
 # load(here("code", "deploy_app_k09", ".deploy_info.Rdata"), verbose = TRUE)
@@ -15,16 +17,13 @@ options(repos = BiocManager::repositories())
 
 ## Deploy the app, that is, upload it to shinyapps.io
 rsconnect::deployApp(
-    appDir = here("code", "deploy_app_k09"),
+    appDir = here("code", "05_shiny"),
     appFiles = c(
         "app.R",
-        "spe_subset_for_spatialLIBD.rds",
-        "sce_pseudo_BayesSpace_k09.rds",
-        "modeling_results_BayesSpace_k09.Rdata",
-        "sig_genes_subset_k09.Rdata",
-        withr::with_dir(here("code", "deploy_app_k09"), dir("www", full.names = TRUE))
+        "Visium_LS_spe.rds",
+        withr::with_dir(here("code", "05_shiny"), dir("www", full.names = TRUE))
     ),
-    appName = "spatialDLPFC_Visium_Sp09",
+    appName = "Visium Lateral Septum",
     account = "libd",
     server = "shinyapps.io"
 )
