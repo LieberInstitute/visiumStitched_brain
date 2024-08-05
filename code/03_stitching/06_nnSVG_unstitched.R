1ed0c90e0db9dca1a37e6dc2b471162e8ce1c6f0
Nick-Eagles <nick.eagles@libd.org>
2024-08-05 15:13:02 -0400

    Add a pair of scripts to run nnSVG as if the SPE object were never stitched

---
+++ code/03_stitching/06_nnSVG_unstitched.R
@@ -0,0 +1,71 @@
+library(SpatialExperiment)
+library(here)
+library(tidyverse)
+library(sessioninfo)
+library(HDF5Array)
+library(Matrix)
+library(nnSVG)
+
+spe_dir <- here("processed-data", "03_stitching", "spe")
+
+message(Sys.time(), " | Loading SpatialExperiment")
+spe <- loadHDF5SummarizedExperiment(spe_dir)
+sample_id <- levels(spe$capture_area)[
+    as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
+]
+
+out_path <- here(
+    "processed-data", "03_stitching", "nnSVG_out_unstitched",
+    paste0(sample_id, ".csv")
+)
+
+set.seed(0)
+dir.create(dirname(out_path), showWarnings = FALSE)
+
+#-------------------------------------------------------------------------------
+#   Subset to this sample and bring into memory for speed
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), " | Subsetting to this capture area and bringing into memory")
+spe <- spe[, spe$capture_area == sample_id]
+assays(spe) <- list(
+    counts = as(assays(spe)$counts, "dgCMatrix"),
+    logcounts = as(assays(spe)$logcounts, "dgCMatrix")
+)
+
+#-------------------------------------------------------------------------------
+#   Use unstitched spatial coordinates
+#-------------------------------------------------------------------------------
+
+spatialCoords(spe)[,'pxl_col_in_fullres'] = as.integer(
+    spe$pxl_col_in_fullres_original
+)
+spatialCoords(spe)[,'pxl_row_in_fullres'] = as.integer(
+    spe$pxl_row_in_fullres_original
+)
+
+#-------------------------------------------------------------------------------
+#   Filter lowly expressed and mitochondrial genes
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), " | Filtering genes and spots")
+spe <- filter_genes(
+    spe,
+    filter_genes_ncounts = 3,
+    filter_genes_pcspots = 0.5,
+    filter_mito = TRUE
+)
+message("Dimensions of spe after filtering:")
+print(dim(spe))
+
+#-------------------------------------------------------------------------------
+#   Run nnSVG and export results
+#-------------------------------------------------------------------------------
+
+message(Sys.time(), " | Running nnSVG")
+spe <- nnSVG(spe)
+
+message(Sys.time(), " | Exporting results")
+write_csv(rowData(spe), out_path)
+
+session_info()
